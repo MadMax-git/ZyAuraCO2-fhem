60cc9381fcdb043554d5cb8a83602eff7bcc8a37
MadMax-git <dogehtsweiter@aol.com>
2017-01-18 17:09:00 +0100

    placeholder for fhem module 74_ZyAuraCO2.pm added.

---
+++ fhem/74_ZyAuraCO2.pm
@@ -0,0 +1,33 @@
+###############################################################################
+#
+#  (c) 2016-2017 Copyright: Joachim Scharnagl
+#  All rights reserved
+#
+#  This script is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  any later version.
+#
+#  The GNU General Public License can be found at
+#  http://www.gnu.org/copyleft/gpl.html.
+#  A copy is found in the textfile GPL.txt and important notices to the license
+#  from the author is found in LICENSE.txt distributed with these scripts.
+#
+#  This script is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+###############################################################################
+
+
+package main;
+
+use strict;
+use warnings;
+use POSIX;
+
+use Blocking;
+
+my $version = "0.0.0";
+
